5df505c1350e48b8f54438c39e68b7d6631e0f97
claramilo290-cyber <claramilo290@gmail.com>
2026-08-22 22:34:29 +0200

    Update base_personnes.js

--- ordinateur/base_personnes.js
+++ ordinateur/base_personnes.js
@@ -1,5 +1,64 @@
 window.personnes = [
 
+    {
+        id: 1,
+        nom: "Belani",
+        prenom: "Ryan",
+        dateNaissance: "2009-04-12",
+        lieuNaissance: "Narbonne",
+        age: 17,
+        adresse: "Les Tilleuls",
+        ville: "Les Tilleuls",
+        departement: "Aude",
+        profession: "Lycéen",
+        telephone: "+39 12 34 56 78 90",
+
+        vehicules: [
+            {
+                marque: "Apple",
+                modele: "iPhone 17 Pro Max",
+                couleur: "Titane noir",
+                immatriculation: "",
+                proprietaire: "Non concerné"
+            }
+        ],
+
+        antecedents: "Aucun",
+        recherche: "Non",
+        motifRecherche: ""
+    },
+
+
+    {
+        id: 215,
+        nom: "Robert",
+        prenom: "Julien",
+        dateNaissance: "1985-08-21",
+        lieuNaissance: "Toulouse",
+        age: 41,
+        adresse: "Rue Alsace Lorraine",
+        ville: "Toulouse",
+        departement: "Haute-Garonne",
+        profession: "Ancien agent de sécurité",
+        telephone: "+39 65 10 98 32 76",
+
+        vehicules: [
+            {
+                marque: "BMW",
+                modele: "X3",
+                couleur: "Noire",
+                immatriculation: "MN-215-OP",
+                proprietaire: "Oui"
+            }
+        ],
+
+        antecedents: "Dégradation ancienne",
+        recherche: "Oui",
+        motifRecherche: "Autre affaire, sans lien avec le dossier principal"
+    }
+
+];window.personnes = [
+
 {
 id:1,
 nom:"Belani",
