48676394393d05b5872894e762d73742def91e21
claramilo290-cyber <claramilo290@gmail.com>
2026-08-23 15:06:15 +0200

    essaie base_personnes.js

--- ordinateur/base_personnes.js
+++ ordinateur/base_personnes.js
@@ -1,33 +1,96 @@
 window.personnes = [
 
-    {
-        id: 1,
-        nom: "Belani",
-        prenom: "Ryan",
-        dateNaissance: "2009-04-12",
-        lieuNaissance: "Narbonne",
-        age: 17,
-        adresse: "Les Tilleuls",
-        ville: "Les Tilleuls",
-        departement: "Aude",
-        profession: "Lycéen",
-        telephone: "+39 12 34 56 78 90",
+   {
+    id: 1,
+    nom: "Belani",
+    prenom: "Ryan",
+    dateNaissance: "2004-04-14",
+    lieuNaissance: "Narbonne (11)",
+    nationalite: "Française",
+    age: 20,
+    adresse: "23 rue du Général de Gaulles",
+    ville: "Saint-Laurent-des-Vignes",
+    departement: "Aude",
+    situationFamiliale: "Célibataire sans enfant",
 
-        vehicules: [
-            {
-                marque: "Apple",
-                modele: "iPhone 17 Pro Max",
-                couleur: "Titane noir",
-                immatriculation: "",
-                proprietaire: "Non concerné"
-            }
-        ],
+    profession: "Etudiant en medecine 2e année",
+    lieuDeTravail: "Faculté De Médecine Montpellier-Nîmes : Campus Arnaud de Villeneuve",
 
-        antecedents: "Aucun",
-        recherche: "Non",
-        motifRecherche: ""
-    },
+    telephone: "+39 12 34 56 78 90",
 
+    appareils: [
+        {
+            type: "Téléphone",
+            marque: "Iphone",
+            modele: "17 pro max",
+            couleur: "Bleu nuit",
+            stockage: "256 Go"
+        }
+    ],
+
+    vehicules: [
+        {
+            type: "",
+            marque: "",
+            modele: "",
+            couleur: "",
+            immatriculation: "",
+            proprietaire: "",
+            assure: "",
+            controleTechnique: ""
+        }
+    ],
+
+    antecedents: "Aucun",
+    recherche: "Non",
+    motifRecherche: ""
+},
+
+       {
+    id: 2,
+    nom: "Moreau",
+    prenom: "Camille",
+    dateNaissance: "2004-04-14",
+    lieuNaissance: "Montpellier (34)",
+    nationalite: "Française",
+    age: 22,
+    adresse: "18 rue des Acacias",
+    ville: "Saint-Laurent-des-Vignes",
+    departement: "Aude",
+    situationFamiliale: "Célibataire sans enfant",
+
+    profession: "Assistante sociale",
+    lieuDeTravail: "Service social territorial de Saint-Laurent-des-Vignes",
+
+    telephone: "+39 92 34 56 68 80",
+
+    appareils: [
+        {
+            type: "Téléphone",
+            marque: "Samsung",
+            modele: "Galaxy S25 FE",
+            couleur: "Violet",
+            stockage: "256 Go"
+        }
+    ],
+
+    vehicules: [
+        {
+            type: "voiture",
+            marque: "Peugeot",
+            modele: "308",
+            couleur: "Bleu Lagoa MOKH",
+            immatriculation: "ZP-376-HS",
+            proprietaire: "Camille Moreau",
+            assure: "Oui",
+            controleTechnique: "27/10/2028"
+        }
+    ],
+
+    antecedents: "Aucun",
+    recherche: "Non",
+    motifRecherche: ""
+},
     {
         id: 215,
         nom: "Robert",
